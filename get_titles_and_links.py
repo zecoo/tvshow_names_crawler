a8f562f764a180a24d75baf016d8fc1c07ae72fc
zecoo <2782596816@qq.com>
2019-01-30 13:44:59 +0800

    increase number to 200+

--- get_titles_and_links.py
+++ get_titles_and_links.py
@@ -3,27 +3,25 @@ import urllib
 import requests
 
 # Get top tvshows from "mezjutt.com"
-url = "https://www.meijutt.com/alltop_hit.html"
-html = requests.get(url)
-# Chinese titles included
-html.encoding = "GB2312"
-data = html.text
-
-# Plz don't use BeautifuleSoup(data, "lxml") because lxml didn't work
-# test = urllib.urlopen(url).read().decode("GB2312")
-# print test
+pageNo = 1
 
 # new .txt written in the directory
 titles = open("titles.txt", 'w')
 links = open("links.txt", 'w')
+for pageNo in range(1, 1000):
+	url = "https://www.meijutt.com/" + str(pageNo) + "_______hit.html"
+	print url
+	html = requests.get(url)
+	html.encoding = "GB2312"
+	data = html.text
+	soup = BeautifulSoup(data, "html.parser")
+	tvshows = soup.find_all('a', {'class': 'B font_14'})
+	for show in tvshows:
+		title = show.string.encode('utf-8').strip() + '\n'
+		link = show['href'].encode('utf-8').strip() + '\n'
+		titles.write(title)
+		links.write(link)
 
-soup = BeautifulSoup(data, "html.parser")
-tvshows = soup.find_all('a', {'target': '_blank'})
-for show in tvshows:
-	title = show.string.encode('utf-8').strip() + '\n'
-	link = show['href'].encode('utf-8').strip() + '\n'
-	titles.write(title)
-	links.write(link)
 
 titles.close()
 links.close()
